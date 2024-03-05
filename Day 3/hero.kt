67d399e5135f98e4f05a213161f9af758134af8b
samy utomo <samyutomo@gmail.com>
2024-03-05 20:38:42 +0700

    Day 3/366

---
+++ Day 3/hero.kt
@@ -0,0 +1,9 @@
+fun main() {
+    print(hero(10,5))
+}
+
+// Solution logic: the bullets carried must be twice the number of dragons
+val hero: (Int, Int) -> Boolean = { bullets: Int, dragons: Int -> dragons*2 <= bullets }
+
+// Solution with bitwise operations
+fun hero2(bullets: Int, dragons: Int): Boolean = dragons shl 1 <= bullets
